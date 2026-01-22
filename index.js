9b9723354ef1de22f40cf176aeb6b81cd212be70
Nav-2003 <88257725+Nav-2003@users.noreply.github.com>
2026-01-22 21:25:07 +0530

    Update Socket.IO server configuration

--- index.js
+++ index.js
@@ -41,10 +41,14 @@ app.get('/api',async (req,res)=>{
 });
 
 const server= http.createServer(app);
-const io=new Server(server,{
-    cors:{origin:"*"}
-})
-
+const io = new Server(server, {
+  path: "/socket.io",
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+  transports: ["websocket"]
+});
 emailSocket(io);
 
 export {io}
