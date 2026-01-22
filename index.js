2c912caf45dd89a6cff0720339d2a7108e410764
Nav-2003 <88257725+Nav-2003@users.noreply.github.com>
2026-01-22 15:11:44 +0530

    Change CORS origin from localhost to production URL

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app=express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://localservices.publicvm.com",
     credentials:true
 }));
 
